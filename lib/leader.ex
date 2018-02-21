f01f350dd60170893276e785501fc82f80611e65
isailaandrei <isailaandrei.i@gmail.com>
2018-02-21 13:29:34 +0000

    Made function to spawn Scout and Commander and send message to monitor

--- lib/leader.ex
+++ lib/leader.ex
@@ -3,8 +3,8 @@ defmodule Leader do
   def start config, monitor do
     receive do
       {:bind, acceptors, replicas} ->
-        spawn Scout, :start, [self(), acceptors, {0, self()}]
-        send monitor, {:add_scout, config.server_num}
+        ballot_num  = {0, self()}
+        spawn_send_scout acceptors, ballot_num, config, monitor
         next acceptors, replicas, {0, self()}, false, Map.new, config, monitor
     end
   end
@@ -17,8 +17,7 @@ defmodule Leader do
         proposals =
           if (proposals[s] == nil || proposals[s] == c) do
             if active do
-              spawn Commander, :start, [self(), acceptors, replicas, {ballot_num, s, c}]
-              send monitor, {:add_commander, config.server_num}
+              spawn_send_commander acceptors, replicas, {ballot_num, s, c}, config, monitor
             end
             Map.put(proposals, s, c)
           else
@@ -30,8 +29,7 @@ defmodule Leader do
       {:adopted, ballot_num, pvals} ->
         proposals = rightjoin proposals, pvals
         for {s, c} <- proposals, do:
-         spawn Commander, :start, [self(), acceptors, replicas, {ballot_num, s, c}]
-         send monitor, {:add_commander, config.server_num}
+         spawn_send_commander acceptors, replicas, {ballot_num, s, c}, config, monitor
         active = true
         next acceptors, replicas, ballot_num, active, proposals, config, monitor
 
@@ -39,8 +37,7 @@ defmodule Leader do
         {:preempted, b={r_app, _}} ->
           {ballot_num, active} =
             if b > ballot_num do
-              spawn Scout, :start, [self(), acceptors, {r_app + 1, self()}]
-              send monitor, {:add_scout, config.server_num}
+              spawn_send_scout acceptors, {r_app + 1, self()}, config, monitor
               {{r_app + 1, self()}, false}
             else
               {ballot_num, active}
@@ -62,6 +59,16 @@ defmodule Leader do
     res = Map.new(Map.to_list(pvals) ++
   Enum.filter(proposals, fn {s, _} -> !Map.has_key?(pvals, s) end))
     res
+  end
+
+  def spawn_send_scout acceptors, ballot_num, config, monitor  do
+    spawn Scout, :start, [self(), acceptors, ballot_num]
+    send monitor, {:add_scout, config.server_num}
+  end
+
+  def spawn_send_commander acceptors, replicas, pvalue, config, monitor do
+    spawn Commander, :start, [self(), acceptors, replicas, pvalue]
+    send monitor, {:add_commander, config.server_num}
   end
 
 
